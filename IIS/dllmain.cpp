1db99b1e938c890b03b43d441054045b0b6fe073
caixukun <jackie.zmy@gmail.com>
2020-12-25 23:15:34 +0800

    修改代码

--- IIS/dllmain.cpp
+++ IIS/dllmain.cpp
@@ -6,60 +6,8 @@ IHttpServer* g_pHttpServer = NULL;  //  Global server instance
 PVOID g_pModuleContext = NULL;  //  Global module context id
 CRITICAL_SECTION g_CS;
 static const DWORD BUFFER_LEN = 256;
-const char* base64char = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-const char padding_char = '=';
 static TCHAR* DEFAULT_HEADER_NAME = _T("X-Forwarded-For");
 TCHAR gHEADER_NAME[BUFFER_LEN];
-
-
-
-int base64_encode(const unsigned char* sourcedata, char* base64)
-{
-	int i = 0, j = 0;
-	unsigned char trans_index = 0; // 索引是8位，但是高两位都为0
-	const int datalength = strlen((const char*)sourcedata);
-	for (; i < datalength; i += 3) {
-		// 每三个一组，进行编码
-		// 要编码的数字的第一个
-		trans_index = ((sourcedata[i] >> 2) & 0x3f);
-		base64[j++] = base64char[(int)trans_index];
-		// 第二个
-		trans_index = ((sourcedata[i] << 4) & 0x30);
-		if (i + 1 < datalength) {
-			trans_index |= ((sourcedata[i + 1] >> 4) & 0x0f);
-			base64[j++] = base64char[(int)trans_index];
-		}
-		else {
-			base64[j++] = base64char[(int)trans_index];
-
-			base64[j++] = padding_char;
-
-			base64[j++] = padding_char;
-
-			break; // 超出总长度，可以直接break
-		}
-		// 第三个
-		trans_index = ((sourcedata[i + 1] << 2) & 0x3c);
-		if (i + 2 < datalength) { // 有的话需要编码2个
-			trans_index |= ((sourcedata[i + 2] >> 6) & 0x03);
-			base64[j++] = base64char[(int)trans_index];
-
-			trans_index = sourcedata[i + 2] & 0x3f;
-			base64[j++] = base64char[(int)trans_index];
-		}
-		else {
-			base64[j++] = base64char[(int)trans_index];
-
-			base64[j++] = padding_char;
-
-			break;
-		}
-	}
-
-	base64[j] = '\0';
-
-	return 0;
-}
 
 BOOL IsSpider(const char* szUa) {
 
